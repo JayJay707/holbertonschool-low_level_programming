9bfa9680666dd1bfa22850cb4d4754a07c556982
JayJay707 <juliette.mesnil35@gmail.com>
2022-02-04 17:40:35 +0100

    Added task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- *main - Prints numbers 0-9
+ *main - Prints numbers 0-9 with putchar
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int n = 0;
+	int n = '0';
 
-	while (n < 10)
+	while (n <= '9')
 	{
-		printf("%i", n);
+		putchar(n);
 		n++;
 	}
 	putchar('\n');
