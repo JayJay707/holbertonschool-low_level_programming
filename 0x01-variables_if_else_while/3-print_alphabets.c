8fca14033894aa0ad455c420c7b98a28c900427d
JayJay707 <juliette.mesnil35@gmail.com>
2022-02-04 17:13:15 +0100

    Fixed task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,20 +9,20 @@ int main(void)
 {
 	char l = 'a';
 	char u = 'A';
-	
+
 	while (l <= 'z')
 	{
 		putchar(l);
 		l++;
 	}
-	
+
 	while (u <= 'Z')
 	{
 		putchar(u);
 		u++;
 	}
-	
+
 	putchar('\n');
-	
+
 	return (0);
 }
