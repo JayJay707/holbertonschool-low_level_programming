1ddc29c61a1c33b5501f2b407035bc149c8e579f
JayJay707 <juliette.mesnil35@gmail.com>
2022-02-08 14:00:06 +0100

    Added task 2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,4 +18,11 @@ int _putchar(char c);
 
 void print_alphabet(void);
 
+/**
+ * print_alphabet_x10 - Prints lowercase alphabet 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void);
+
 #endif
