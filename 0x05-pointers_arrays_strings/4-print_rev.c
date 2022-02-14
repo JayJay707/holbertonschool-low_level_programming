8c9e18f4d137a28be5f804adde3c02c711c876c1
JayJay707 <juliette.mesnil35@gmail.com>
2022-02-14 15:25:45 +0100

    Added task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: String
+ */
+
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
