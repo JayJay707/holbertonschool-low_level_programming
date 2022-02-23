b6ebbd780729416bd9e61498194bdfb9a520c54f
JayJay707 <juliette.mesnil35@gmail.com>
2022-02-23 13:24:23 +0100

    Update task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,9 +2,10 @@
 
 /**
  * _pow_recursion - Returns the value of x raised to the power of y
- * @x: value to multiply
- * @y: power
- * Return: int
+ * @x: the value to multiply
+ * @y: the times to multiply the value
+ *
+ * Return: the value multiplied y times
  */
 
 int _pow_recursion(int x, int y)
@@ -15,5 +16,5 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 		return (1);
 
-	return (x * _pow_recursion(x, y - 1);
+	return (x * _pow_recursion(x, y - 1));
 }
