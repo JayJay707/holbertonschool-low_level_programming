5d121981eefbe374a704faf4526c7cf6a72a38de
JayJay707 <juliette.mesnil35@gmail.com>
2022-09-12 13:52:40 +0200

    Fixed task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -18,7 +18,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (left <= right)
 	{
-		mid = left + right / 2;
+		mid = (left + right) / 2;
 		printf("Searching in array: ");
 
 		for (i = left; i < right; i++)
