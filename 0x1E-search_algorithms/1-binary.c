294fe6ce040689d9401d0e4a9c1659e97d6a988d
JayJay707 <juliette.mesnil35@gmail.com>
2022-09-12 13:34:26 +0200

    Remade task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,42 +1,37 @@
 #include "search_algos.h"
 
 /**
- * binary_search - implement  binary search  algo
- * @array: given array to search in
+ * binary_search - searches for a value using binary search
+ * @array: array to search in
  * @size: size of the array
  * @value: value to search for
- * Return: first index or -1
+ * Return: first index at which the value is foundor -1
  */
 
 int binary_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
-	size_t left = 0, right = size - 1, midlle;
+	size_t left = 0, right = size - 1, mid;
 
 	if (array == NULL)
 		return (-1);
+
 	while (left <= right)
 	{
+		mid = left + right / 2;
 		printf("Searching in array: ");
+
 		for (i = left; i < right; i++)
 		{
-			printf("%i, ", array[i]);
-		}
-		printf("%i\n", array[right]);
-		midlle = (left + right) / 2;
-		if (array[midlle] < value)
-		{
-			left = midlle + 1;
-		}
-		else if (array[midlle] > value)
-		{
-			right = midlle - 1;
-		}
-		else
-		{
-			return (midlle);
+			printf("%i", array[i]);
 		}
 
+		if (array[mid] < value)
+			left = mid + 1;
+		else if (array[mid] > value)
+			right = mid - 1;
+		else
+			return (mid);
 	}
 	return (-1);
 }
